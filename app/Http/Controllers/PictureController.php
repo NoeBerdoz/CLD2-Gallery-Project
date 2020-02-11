7e44abba5209acc5763be05850cd69a468cbbb4e
NoeBerdoz <44669187+NoeBerdoz@users.noreply.github.com>
2020-02-11 09:55:44 +0100

    Show pictures in vue

--- app/Http/Controllers/PictureController.php
+++ app/Http/Controllers/PictureController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Requests\PictureRequest;
 use App\Picture;
 use Illuminate\Http\Request;
+use Illuminate\Session\Store;
+use Illuminate\Support\Facades\Storage;
 
 class PictureController extends Controller
 {
@@ -53,9 +55,13 @@ class PictureController extends Controller
      * @param  \App\Picture  $picture
      * @return \Illuminate\Http\Response
      */
-    public function show(Picture $picture)
+    public function show(Request $request,Picture $picture)
     {
-        //
+        if (\Str::startsWith($request->header('Accept'), 'image')){
+            return \Storage::get($picture->storage_path);
+        }
+
+        return view('pictures.show', compact('picture'));
     }
 
     /**
